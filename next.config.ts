cbdb8a66d51d7c9060e0c86ba3d3b0f12fa97b23
nandkishorechhimpa <nandkishore.chhimpa@wearecyborg.com>
2025-10-07 17:34:51 +0530

    Added headers to allow CSP Xframe to show chatbot in iframe for testing

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   async headers() {
+    return [
+      {
+        source: "/(.*)", // all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors http://localhost:3000/", // replace with your company domain
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM http://localhost:3000/", // optional fallback
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
